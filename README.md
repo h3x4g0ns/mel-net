9a6a32a408c86cc278f5976747b71a9678de8bb3
Ekansh Agrawal <agrawalekansh02@gmil.com>
2022-06-09 20:19:24 -0700

    added modified vgg

--- README.md
+++ README.md
@@ -6,8 +6,8 @@ We know that Neural Networks can do a pretty good job learning latent informatio
 
 For the sake of consistency, I'll be running tests with the [spoken_digit](https://www.tensorflow.org/datasets/catalog/spoken_digit) dataset which is basically MNIST but with audio. 
 
-- [ ] vanilla CNN
-- [ ] auto encoder into MLP
+- [ ] modified VGG19
+- [ ] autoencoder into MLP
 - [ ] modified ResNet
 
 For a more complicated dataset, I will use RNN cells + ResNet to predict words in a sequence with the [crema_d](https://www.tensorflow.org/datasets/catalog/crema_d) audo-visual dataset. This dataset consists of facial and vocal emotional expression in sentences spoken in a range of basic emotion states.
---
+++ models.py
@@ -0,0 +1,40 @@
+from sklearn.semi_supervised import SelfTrainingClassifier
+import tensorflow as tf
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from torch import dropout
+
+def ModifiedVGG(Model):
+    def __init__(self, layer_activation, final_activation, dropout_rate, latent_dim, num_classes):
+        super(ModifiedVGG, self).__init__()
+
+        self.al = layer_activation
+        self.af = final_activation
+        self.d = dropout_rate
+        self.l = latent_dim
+        self.nc = num_classes
+
+
+    def build(self):
+        self.conv = Sequential()
+        for filters, num_layers in zip([64, 128, 256], [2, 2, 3]):
+            for _ in range(num_layers):
+                self.conv.add(Conv2D(filters, (3, 3), padding='same', activation=self.al))
+            self.conv.add(MaxPooling2D((2, 2)))
+            self.conv.add(Dropout(self.d))
+
+        self.flatten = Flatten()
+        self.fc1 = Dense(self.l, activation=self.al)
+        self.fc2 = Dense(self.l, activation=self.al)
+        self.o = Dense(self.nc, activation=self.af)
+
+    def call(self, input):
+        x = self.conv(input)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.fc2(x)
+        x = self.o(x)
+        return x
+
+
+
